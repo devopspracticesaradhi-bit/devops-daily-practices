280f140de40a7b6e19011d65a695b52c6ded44f3
saradhi <bollapragada@mplgaming.com>
2025-12-19 15:33:02 +0530

    adding gitignore file

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -2,36 +2,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Fetch latest AMI ID for Amazon Linux 2
-data "aws_ami" "name" {
+# Fetch latest Amazon Linux 2 AMI
+data "aws_ami" "latest" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
 }
 
+# Fetch default VPC
 data "aws_vpc" "default" {
   default = true
-  
 }
 
-data "aws_subnet_ids" "default" {
+# Fetch default subnets
+data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
 
-
-
+# Create EC2 instance
 resource "aws_instance" "demo" {
-  ami           = data.aws_ami.name.id
+  ami           = data.aws_ami.latest.id
   instance_type = var.instance_type
   subnet_id     = data.aws_subnets.default.ids[0]
 
--- terraform-practice/output.tf
+++ terraform-practice/output.tf
@@ -1,17 +1,15 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-  
-}
-
-output "aws_subnet_ids" {
-  value = aws_subnet.main[*].id
-  
-}
-
 output "instance_public_ip" {
   value = aws_instance.demo.public_ip
 }
 
 output "instance_id" {
   value = aws_instance.demo.id
+}
+
+output "vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "subnet_ids" {
+  value = data.aws_subnets.default.ids
 }
