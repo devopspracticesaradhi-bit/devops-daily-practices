ddd9fba74ab2e6ea2b3282b2568f2b41faa415ac
saradhi <bollapragada@mplgaming.com>
2025-12-19 19:16:38 +0530

    provider tf file and remote state

--- terraform-practice/provider.tf
+++ terraform-practice/provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 1.11.0"
     backend "s3" {
         bucket = "terraform-practice-bucket-saradhi"
-        key    = "terraform-practice-bucket-saradhi/terraform-practice/"
+        key    = "terraform-practice-bucket-saradhi/terraform-practice"
         region = "us-east-1"
         encrypt = true 
         use_lockfile = true
