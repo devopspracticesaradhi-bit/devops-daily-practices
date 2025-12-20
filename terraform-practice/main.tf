f521c38cde8012a06065cbfa1ab12798206d41d4
saradhi <bollapragada@mplgaming.com>
2025-12-20 20:27:37 +0530

    provider tf file and remote state

--- terraform-practice/main.tf
+++ terraform-practice/main.tf
@@ -61,7 +61,7 @@ resource "null_resource" "remote_setup" {
     connection {
       type = "ssh"
       user = "ec2-user"
-      private_key = aws_key_pair.saradhi.key_pair_id
+      private_key = tls_private_key.saradhi.private_key_pem
       host        = aws_instance.demo.public_ip
     }
 
